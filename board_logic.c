a6f168fb91fb7f7f56c4f80f5e22eb9bd4b11f4a
romir kulshrestha <romir.kulshrestha@gmail.com>
2024-07-15 01:26:31 +0200

    clean (taylors version)

--- board_logic.c
+++ board_logic.c
@@ -4,78 +4,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-// https://stackoverflow.com/a/4024086
-// Yes, I know how it works
-// No, I could not have figured it out by myself :(
-//
-// I'm realizing it's not the best idea to have stuff which changes size constantly
-// for sake of avoiding future headaches. no stack for home
-//
-// PAWN STATES- at home, final row, outer loop, finished
-//
-// #define push(sp, n) (*((sp)++) = (n))
-// #define pop(sp) (*--(sp))
-
 game_state
 create_game() {
     // this is the concise way to write it
     return (game_state){.move_counter = 0, .positions = {0}, .states = {HOME}};
 }
 
-// this whole thing is redundant
-void
-init_board(game_state _) {
-    return;
-
-    // x.move_counter = 0;
-    // x.Y_pos = (pawn_pos *)malloc(sizeof(pawn_pos));
-    // x.R_pos = (pawn_pos *)malloc(sizeof(pawn_pos));
-    // x.G_pos = (pawn_pos *)malloc(sizeof(pawn_pos));
-    // x.B_pos = (pawn_pos *)malloc(sizeof(pawn_pos));
-    // // there has to be a more concise way to write this part
-    // pawn_states init_stack[4] = {HOME, HOME, HOME, HOME};
-    // x.Y_pawns = memcpy(x.Y_pawns, init_stack, sizeof(init_stack));
-    // x.R_pawns = memcpy(x.R_pawns, init_stack, sizeof(init_stack));
-    // x.G_pawns = memcpy(x.G_pawns, init_stack, sizeof(init_stack));
-    // x.B_pawns = memcpy(x.B_pawns, init_stack, sizeof(init_stack));
-}
-
 Color
 whose_turn_is_it(game_state g) {
     return g.move_counter % 4;
 }
-
-// turn
-// whose_turn_is_it(game_state l)
-// {
-//     turn *out = NULL;
-//     switch(l.move_counter % 4)
-//     {
-//         case 0:
-//             out->curr_pawns = l.Y_pawns;
-//             out->curr_positions = l.Y_pos;
-//             out->cc = Yellow;
-//             break;
-//         case 1:
-//             out->curr_pawns = l.R_pawns;
-//             out->curr_positions = l.R_pos;
-//             out->cc = Green;
-//             break;
-//         case 2:
-//             out->curr_pawns = l.G_pawns;
-//             out->curr_positions = l.G_pos;
-//             out->cc = Red;
-//             break;
-//         case 3:
-//             out->curr_pawns = l.B_pawns;
-//             out->curr_positions = l.B_pos;
-//             out->cc = Blue;
-//             break;
-//     }
-//     return *out;
-// }
-
 
 // this is supposed to be a frontend call which decides whether the
 // player has selected HOMEROW (0) or Pawns (anything that's not HOME or
@@ -94,7 +32,8 @@ getRandomNumber() {
 void
 play(game_state g) {
     Color curr = whose_turn_is_it(g);
-    int roll = rand() % 5 + 1;
+    uint8_t roll = rand() % 5 + 1;
+
     uint8_t *positions = get_positions(g, curr);
     pawn_states *states = get_states(g, curr);
 
@@ -104,65 +43,23 @@ play(game_state g) {
         if (!move_p) {
             int release_pawn = get_offset(curr); // this is stupid and I can't decide if I hate
                                                  // abstraction more than nesting
-            int a = 0;
-            for (; a < 4; a++) {
-                if (states[a] == HOME)
-                    break; // first home pawn is released
+            for (int a = 0; a < 4; a++) {
+                if (states[a] == HOME) {
+                    positions[a] = release_pawn;
+                    states[a] = OUTER;
+                    break;
+                }
             }
-
-            // unnecessary abstraction
-            {
-                // release pawn and also change it's state from HOME to OUTER
-                // switch (a) {
-                // case 0:
-                //     curr.curr_positions->p1 = release_pawn;
-                //     curr.curr_pawns[0] = OUTER;
-                //     break;
-                // case 1:
-                //     curr.curr_positions->p2 = release_pawn;
-                //     curr.curr_pawns[1] = OUTER;
-                //     break;
-                // case 2:
-                //     curr.curr_positions->p3 = release_pawn;
-                //     curr.curr_pawns[2] = OUTER;
-                //     break;
-                // case 3:
-                //     curr.curr_positions->p4 = release_pawn;
-                //     curr.curr_pawns[3] = OUTER;
-                //     break;
-                // }
-            }
-
-            // this is the same as the switch statement above
-            positions[a] = release_pawn;
-            states[a] = OUTER;
 
             // but what happens if there are no pawns at home?
             // no case to handle a == 4
             // TODO: handle this case
-
-        } else {
-            // for now there are no transitions out of the OUTER loop so there's no
-            // FINAL or ASCENDED states so no state changes after HOME to OUTER
-
-            // switch (move_p) {
-            // case 0:
-            //     curr.curr_positions->p1 += 6;
-            //     break;
-            // case 1:
-            //     curr.curr_positions->p2 += 6;
-            //     break;
-            // case 2:
-            //     curr.curr_positions->p3 += 6;
-            //     break;
-            // case 3:
-            //     curr.curr_positions->p4 += 6;
-            //     break;
-            // }
-
-            // this is the same as the switch statement above
-            positions[move_p] += 6;
+            return;
         }
+        // for now there are no transitions out of the OUTER loop so there's no
+        // FINAL or ASCENDED states so no state changes after HOME to OUTER
+
+        positions[move_p] += 6;
     }
 }
 
--- state.h
+++ state.h
@@ -1,20 +1,12 @@
 
 #include <stdint.h>
 
-// #define YELLOW_OFFSET   0
-// #define GREEN_OFFSET    11
-// #define RED_OFFSET      21
-// #define BLUE_OFFSET     35
-
-// this is fine, but I'd rather use an enum for this
-
 typedef enum {
     YELLOW_OFFSET = 0,
     GREEN_OFFSET = 11,
     RED_OFFSET = 21,
     BLUE_OFFSET = 35
 } Offset;
-
 
 // fun facts about enums!
 // https://kubyshkin.name/posts/c-language-enums-tips-and-tricks/
@@ -23,30 +15,14 @@ typedef enum {
     FINAL,
     OUTER,
     ASCENDED
-} pawn_states; // as far as naming conventions go, I'd rather use PascalCase
-               // for types
+} pawn_states;
 
 typedef enum {
-    // also inconsistent casing in your enums; the ones above are CAPS and this
-    // one is PascalCase
     Yellow,
     Green,
     Red,
     Blue
 } Color;
-
-// unnecessary abstraction -- you are indexing them anyway and this way you
-// can't use a for loop (that would otherwise be unrolled by the compiler)
-/*
-typedef struct
-{
-    int p1;
-    int p2;
-    int p3;
-    int p4;
-}
-pawn_pos;
-*/
 
 typedef struct {
     uint8_t move_counter;
@@ -54,21 +30,10 @@ typedef struct {
     pawn_states states[4]
 } game_state; // same here, PascalCase
 
-// unnecessary abstraction 2: electric boogaloo
-/*
-typedef struct
-{
-    uint8_t curr_positions[4];
-    pawn_states *curr_pawns;
-    COLOR cc;
-}
-turn;
-*/
-
 // convenience macros -- make them inline functions if you want type safety but
 // I don't think it's necessary
-#define get_positions(state, color) ((state).positions + (color)*4)
-#define get_states(state, color)    ((state).states + (color)*4)
+#define get_positions(state, color) ((state).positions + (color) * 4)
+#define get_states(state, color)    ((state).states + (color) * 4)
 
 inline Offset
 get_offset(Color cc) {
